54128b46da83da62dccc92690088ba97083c6dc3
coffee-bits <126335564+coffee-bits@users.noreply.github.com>
2023-02-27 06:46:00 +0100

    motor control included, untested

--- ArduinoMotorSerialController.ino
+++ ArduinoMotorSerialController.ino
@@ -1,3 +1,21 @@
+/****************************************************************************
+ *   This code is distributed in the hope that it will be useful,           *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ ****************************************************************************/
+
+/**
+ * @file ArduinoMotorSerialController.ino
+ * @author CJ
+ * @date February 2023
+ * @brief This code uses a motor shield with an 74HC595 shift register together
+ * with an L293D Motor controller an 4 motor ports. It 
+ */
+
+
+
 #include <MotorDriver.h>
 
 enum serialCommandSpecifier{
@@ -13,22 +31,24 @@ enum serialCommandFunction{
   SERIAL_COMMAND_FUNCTION_UNDEF
   };
 
+
 enum serialCommandFunctionParameterDirection{
-  SERIAL_COMMAND_FUNCTION_MOTOR_DIRECTION_FORWARD      = 0,
-  SERIAL_COMMAND_FUNCTION_MOTOR_DIRECTION_BACKWARD     = 1,
-  SERIAL_COMMAND_FUNCTION_MOTOR_DIRECTION_STOP         = 2,
-  SERIAL_COMMAND_FUNCTION_MOTOR_DIRECTION_RELEASE      = 3, 
-  SERIAL_COMMAND_FUNCTION_MOTOR_DIRECTION_UNDEF
+  SERIAL_COMMAND_FUNCTION_MOTOR_DIRECTION_FORWARD      = 1,
+  SERIAL_COMMAND_FUNCTION_MOTOR_DIRECTION_BACKWARD     = 2,
+  SERIAL_COMMAND_FUNCTION_MOTOR_DIRECTION_STOP         = 3,
+  SERIAL_COMMAND_FUNCTION_MOTOR_DIRECTION_RELEASE      = 4, 
+  SERIAL_COMMAND_FUNCTION_MOTOR_DIRECTION_UNDEF        = 0
   };
 
 enum serialCommandFunctionParameterMotorSelection{
-  SERIAL_COMMAND_FUNCTION_MOTOR_SELECTION_A     = 0,
-  SERIAL_COMMAND_FUNCTION_MOTOR_SELECTION_B     = 1,
-  SERIAL_COMMAND_FUNCTION_MOTOR_SELECTION_C     = 2,
-  SERIAL_COMMAND_FUNCTION_MOTOR_SELECTION_D     = 3,
-  SERIAL_COMMAND_FUNCTION_MOTOR_SELECTION_M     = 4,
-  SERIAL_COMMAND_FUNCTION_MOTOR_SELECTION_UNDEF
+  SERIAL_COMMAND_FUNCTION_MOTOR_SELECTION_A     = 1,
+  SERIAL_COMMAND_FUNCTION_MOTOR_SELECTION_B     = 2,
+  SERIAL_COMMAND_FUNCTION_MOTOR_SELECTION_C     = 3,
+  SERIAL_COMMAND_FUNCTION_MOTOR_SELECTION_D     = 4,
+  SERIAL_COMMAND_FUNCTION_MOTOR_SELECTION_M     = 9,
+  SERIAL_COMMAND_FUNCTION_MOTOR_SELECTION_UNDEF = 0
   };
+
 
 struct serialCommandMotorSet{
   serialCommandFunctionParameterMotorSelection motorSelection = SERIAL_COMMAND_FUNCTION_MOTOR_SELECTION_UNDEF;
@@ -42,6 +62,7 @@ String serialCommand;
 serialCommandSpecifier MAIN_receiveSerialCommand = SERIAL_COMMAND_UNDEF;
 serialCommandFunction  MAIN_receivedSerialCommandFunction = SERIAL_COMMAND_FUNCTION_UNDEF;
 serialCommandMotorSet  MAIN_receivedSerialMotorSettings;
+
 
 void setup()
 {
@@ -59,12 +80,29 @@ void loop()
   }
   if (validCommandReceived){
     if ( MAIN_receivedSerialCommandFunction == SERIAL_COMMAND_FUNCTION_MOTOR){
-      
+      MotorFunctionExecution();
     }
-      MAIN_motorControl.motor(1,FORWARD,255);
-    }
-
+  }
 }
+
+
+bool MotorFunctionExecution(){
+  if ((MAIN_receivedSerialMotorSettings.motorSelection > 0)
+  && (MAIN_receivedSerialMotorSettings.motorSelection < 5)){
+   MAIN_motorControl.motor(MAIN_receivedSerialMotorSettings.motorSelection,MAIN_receivedSerialMotorSettings.motorDirection,MAIN_receivedSerialMotorSettings.motorSpeed); 
+  }
+  else if (MAIN_receivedSerialMotorSettings.motorSelection == 9){
+    MAIN_motorControl.motor(1, MAIN_receivedSerialMotorSettings.motorDirection,MAIN_receivedSerialMotorSettings.motorSpeed); 
+    MAIN_motorControl.motor(2, MAIN_receivedSerialMotorSettings.motorDirection,MAIN_receivedSerialMotorSettings.motorSpeed); 
+    MAIN_motorControl.motor(3, MAIN_receivedSerialMotorSettings.motorDirection,MAIN_receivedSerialMotorSettings.motorSpeed); 
+    MAIN_motorControl.motor(4, MAIN_receivedSerialMotorSettings.motorDirection,MAIN_receivedSerialMotorSettings.motorSpeed);     
+  }
+  else {
+    return false;
+    }
+  return true;
+}
+
 
 /**
  * @brief check for a valid command\n
@@ -80,7 +118,7 @@ bool CheckForValidCommand(){
   {
     return false;  
   }
-
+  // byte[0] Command: [S]et, G[get], [E]xecute
   switch(serialCommand[0]){
     case 'S':
      MAIN_receiveSerialCommand = serialCommandSpecifier::SERIAL_COMMAND_SET;
@@ -96,8 +134,7 @@ bool CheckForValidCommand(){
      return false;
     break;    
   }
-
-
+  // byte[1] Function/Interface: [M]otor
   switch(serialCommand[1]){
     case 'M':
      MAIN_receivedSerialCommandFunction = SERIAL_COMMAND_FUNCTION_MOTOR;
@@ -107,7 +144,7 @@ bool CheckForValidCommand(){
      return false;
     break;    
   }
-
+  // byte[2] Function/Interface sub specifier: motor [A],[B],[C],[D],[M]ulti selection
   if (MAIN_receivedSerialCommandFunction == SERIAL_COMMAND_FUNCTION_MOTOR){
    switch(serialCommand[2]){
       case 'A':
@@ -130,8 +167,7 @@ bool CheckForValidCommand(){
           return false;  
       break;
     }    
-    
-    
+    // byte[3] Function/Interface parameter: motor direction [F]orward, [B]ackward, [S]top, R[elease]    
     switch(serialCommand[3]){
       case 'F':
           MAIN_receivedSerialMotorSettings.motorDirection = SERIAL_COMMAND_FUNCTION_MOTOR_DIRECTION_FORWARD;
@@ -150,49 +186,47 @@ bool CheckForValidCommand(){
           return false;
       break;
     }
-
-       switch(serialCommand[4]){
-      case '0':
-        MAIN_receivedSerialMotorSettings.motorSpeed = 0;
-      break;
-      case '1':
-        MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*1;
-      break;
-      case '2':
-        MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*2;
-      break;
-      case '3':
-        MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*3;
-      break;
-      case '4':
-        MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*4;
-      break;
-      case '5':
-        MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*5;
-      break;
-      case '6':
-        MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*6;
-      break;
-      case '7':
-        MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*7;
-      break;
-      case '8':
-        MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*8;
-      break;
-      case '9':
-        MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*9;
-      break;                
-      default:
-         MAIN_receivedSerialMotorSettings.motorSpeed = 0;
-         return false;
-      break;
+    // byte[4] Function/Interface parameter: motor speed [0]...[9]
+    switch(serialCommand[4]){
+    case '0':
+      MAIN_receivedSerialMotorSettings.motorSpeed = 0;
+    break;
+    case '1':
+      MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*1;
+    break;
+    case '2':
+      MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*2;
+    break;
+    case '3':
+      MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*3;
+    break;
+    case '4':
+      MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*4;
+    break;
+    case '5':
+      MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*5;
+    break;
+    case '6':
+      MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*6;
+    break;
+    case '7':
+      MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*7;
+    break;
+    case '8':
+      MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*8;
+    break;
+    case '9':
+      MAIN_receivedSerialMotorSettings.motorSpeed = 255/9*9;
+    break;                
+    default:
+       MAIN_receivedSerialMotorSettings.motorSpeed = 0;
+       return false;
+    break;
     }
   }
   else {
     //function not recognize
     return false;
   }
-
   return true;
-
 }
